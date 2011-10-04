d6b423bee3f2656825fdc25f7b1a0d952a56249d
Alex Tucker <tucker.al@gmail.com>
2011-10-03 22:41:28 -0400

    Fleshed out constructor

--- classes/postageapp/core.php
+++ classes/postageapp/core.php
@@ -2,13 +2,23 @@
 
 abstract class Postageapp_Core {
    
-   public static function instance($config = null) {
+   private $api_key	= '';
+   private $host	= 'https://api.postageapp.com';
 
+   public static function instance($config = null, $conf_type = 'default') {
+      if($config == null) {
+	 // Get config from global config
+	 $config = Kohana::config('postageapp')->$conf_type;
+      }
+      return new Postageapp($config);
    }
 
    public function __construct($config) {
-
-
+      foreach($config as $key => $value) {
+	 $this->$key = $value; 
+      }
    }
+
+
 
 }
--- config/postageapp.php
+++ config/postageapp.php
@@ -3,7 +3,8 @@
 return array(
    
    'default' => array(
-   
+      'api_key' => '',
+      'host' => 'https://api.postageapp.com',
    ),
 
 );
